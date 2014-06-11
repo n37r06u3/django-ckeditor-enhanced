c72e4ef813d5e3db19e3c345bb35630c7d9863bd
n37r06u3 <n37r06u3@gmail.com>
2014-06-12 00:06:39 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-ckeditor-enhanced',
+    version = '1.0',
+    keywords = ('django','ckeditor'),
+    description = 'django ckeditor ',
+    license = '',
+
+
+    url = 'https://github.com/n37r06u3/django-ckeditor-enhanced',
+    author = 'n37r06u3',
+    author_email = 'n37r06u3@gmail.com',
+
+
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [''],
+    packages=find_packages(),
+
+)
