8369e88fe4684c2a05943ea5927f48cf96d8b208
n37r06u3 <n37r06u3@siyuantongdeMacBook-Pro.local>
2014-06-17 21:58:27 +0800

    修复MANIFEST.in失效问题

--- setup.py
+++ setup.py
@@ -2,20 +2,30 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-ckeditor-enhanced',
-    version = '1.0',
+    version = '1.2',
     keywords = ('django','ckeditor'),
-    description = 'django ckeditor ',
-    license = '',
-
+    description = 'django-ckeditor-updated with codesnippet plugin support',
 
     url = 'https://github.com/n37r06u3/django-ckeditor-enhanced',
     author = 'n37r06u3',
     author_email = 'n37r06u3@gmail.com',
 
-
-    include_package_data = True,
     platforms = 'any',
-    install_requires = [''],
     packages=find_packages(),
+    
+    install_requires=[
+                      'Django',
+                      ],
 
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
